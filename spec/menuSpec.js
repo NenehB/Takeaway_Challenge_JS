b0fa0577c6e94ed3d4aacc8df14108d98a4d2a4a
NenehB <Neneh.Bojang@digital.justice.gov.uk>
2021-11-19 11:50:41 +0000

    able to select available dishes

--- spec/menuSpec.js
+++ spec/menuSpec.js
@@ -1,16 +1,21 @@
 describe('check if I want to order something', function(){
 
+  beforeEach(function(){
+    menu = new Menu();
+  });
 
   it('list of dishes with prices', function(){
-    menu = new Menu();
-    console.log(menu.list());
-    expect(menu.list()).toContain("Pasta £5");
-    expect(menu.list()).toContain("Kimchi £3");
+    expect(menu.listOfDish().toString()).toContain("Pasta");
   })
+
   it('list of dishes with prices', function(){
-    menu = new Menu();
-    console.log(menu.list());
-    expect(menu.list()).toContain("Kimchi £3");
+    expect(menu.listOfDish().toString()).toContain("Kimchi");
+  })
+
+  it('should be able to select several available dishes', function(){
+    menu.choiceOfDish("Pasta")
+    menu.choiceOfDish("Kimchi")
+    expect(menu.choiceOfDish('Chips')).toEqual(['Pasta', 'Kimchi', 'Chips'])
   })
 
 })
--- src/meanu.js
+++ src/meanu.js
@@ -1,15 +1,19 @@
 class Menu {
   constructor(dishes) {
-  this.dishes = { Pasta: '£5', MacAndCheese: '£4', Kimchi: '£3', Pizza: '£5', Burger: '£3.5' };
-  }
-  list(){
+  this.dishes = { Pasta: '£5', MacAndCheese: '£4', Kimchi: '£3', Pizza: '£5', Burger: '£3.5', Chips: '£1.50' };
+  this.list_of_dish = []
+}
+  listOfDish(){
     let items =  this.dishes
     let dish = []
-    // iterate through key-value gracefully
     for (const [key, value] of Object.entries(items)) {
-      dish.push(`${key} ${value}`); 
+      dish.push([`${key}`, `${value}`]); 
     }
-    return dish.toString();
+    return dish;
+  }
+  choiceOfDish(food_choice){
+    this.list_of_dish.push(food_choice)
+     return this.list_of_dish;
   }
 
 }
